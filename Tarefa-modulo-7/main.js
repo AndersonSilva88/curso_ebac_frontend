85fd0e77bd2acd9bc5cabb8d2e468f94b0ba6b1e
Anderson Oliveira da Silva <anderson.os88@hotmail.com>
2022-09-17 16:50:27 -0300

    update

--- Tarefa-modulo-7/main.js
+++ Tarefa-modulo-7/main.js
@@ -1,21 +1,19 @@
 const form = document.getElementById('form-numeros');
 
-const numeroA = document.getElementById('numero-a');
-const numeroB = document.getElementById('numero-b');
+let numeroA = document.getElementById('numero-a');
+let numeroB = document.getElementById('numero-b');
 
 let formEValido = false;
 
 function validar(numeroA, numeroB) {
     if (numeroB > numeroA) {
-        alert("O número B é maior que o número A");
 
     } else {
-        alert("O número A é menor que o número B");
+        alert("Formulário inválido!!");
     }
 }
 
 form.addEventListener('submit', function (e) {
-    e.preventDefault();
     validar(numeroA.value, numeroB.value);
 
 });
